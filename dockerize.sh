49db7f96852419c81ae9c9f5b8ab14a6d9c655bb
Mykola Makhin <mvmn.inbox@gmail.com>
2023-02-12 01:53:34 +0200

    Sample dockerization of WAR using Tomcat image as a parent.

---
+++ dockerize.sh
@@ -0,0 +1,3 @@
+cp ./Dockerfile ./mvmn.permock.web/target
+docker build -t perpetual_mockery ./mvmn.permock.web/target
+
--- mvmn.permock.web/src/main/java/x/mvmn/permock/PerpetualMockeryApplication.java
+++ mvmn.permock.web/src/main/java/x/mvmn/permock/PerpetualMockeryApplication.java
@@ -2,11 +2,12 @@ package x.mvmn.permock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
-public class PerpetualMockeryApplication {
+public class PerpetualMockeryApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PerpetualMockeryApplication.class, args);
